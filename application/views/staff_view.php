b96c3c04788a2f30f20d8bd561311709c557d065
Vpsolo <1foreverfarever@gmail.com>
2019-04-03 22:25:35 +0600

    03.04:22.25

--- application/views/staff_view.php
+++ application/views/staff_view.php
@@ -23,6 +23,8 @@
 			<?php 
 				foreach ($doctor_collective as $d_c) {
 			?>
+			<div class="col-md-4">
+				
 			<div class="paddding">
 				<div class="content-doc" onclick="location.href='<?php echo base_url().'index.php/pages/doctor/'.$d_c['id']; ?>'">
 					<div class="img" style="background-image: url(<?php echo base_url().'assets/images/photos/'.$d_c['foto'];?>);"></div>
@@ -31,6 +33,7 @@
 						<span><?php echo $d_c['tema_kg'] ?></span>						
 					</div>
 				</div>				
+			</div>
 			</div>
 		<?php } ?>
 		</div>		
